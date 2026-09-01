2fe18eaec353f07962afa34022dd35dcfbd5edeb
bmelicque <bmelicque@gmail.com>
2026-09-01 18:42:32 +0200

    fix functions treated as statements where they should be expressions

--- crates/parser/src/items.rs
+++ crates/parser/src/items.rs
@@ -1,14 +1,14 @@
-use tine_ast as ast;
+use tine_ast::*;
 use tine_common::{diagnostics::DiagnosticKind, locations::Location};
 
 use crate::{tokens::Token, Parser};
 
 impl Parser<'_> {
-    pub fn parse_item(&mut self) -> Option<ast::Item> {
+    pub fn parse_item(&mut self) -> Option<Item> {
         match self.tokens.peek() {
             Some((Ok(Token::Use), _)) => {}
             Some((Ok(Token::Newline), _)) => return None,
-            _ => return self.parse_statement().map(|st| st.into()),
+            _ => return self.parse_statement_item(),
         };
 
         let start_range = self.eat(&[Token::Use]);
@@ -26,21 +26,21 @@ impl Parser<'_> {
             self.error(DiagnosticKind::MissingName, error_loc);
         }
 
-        Some(ast::Item::UseDeclaration(ast::UseDeclaration {
+        Some(Item::UseDeclaration(UseDeclaration {
             loc: Location::merge(start_loc, self.next_loc()),
             relative_count,
-            tree: tree.unwrap_or(ast::UseTree {
+            tree: tree.unwrap_or(UseTree {
                 path: vec![],
                 sub_trees: vec![],
             }),
         }))
     }
 
-    fn parse_use_tree(&mut self) -> Option<ast::UseTree> {
+    fn parse_use_tree(&mut self) -> Option<UseTree> {
         let mut path = Vec::new();
         match self.tokens.peek() {
             Some((Ok(Token::Ident(_)), _)) => {
-                path.push(ast::PathElement(self.parse_identifier()));
+                path.push(PathElement(self.parse_identifier()));
             }
             _ => return None,
         }
@@ -48,7 +48,7 @@ impl Parser<'_> {
             self.tokens.next(); // consume the dot
             match self.tokens.peek() {
                 Some((Ok(Token::Ident(_)), _)) => {
-                    path.push(ast::PathElement(self.parse_identifier()));
+                    path.push(PathElement(self.parse_identifier()));
                 }
                 Some((Ok(Token::LBrace), _)) => break,
                 _ => {
@@ -70,6 +70,17 @@ impl Parser<'_> {
             }
             _ => vec![],
         };
-        Some(ast::UseTree { path, sub_trees })
+        Some(UseTree { path, sub_trees })
+    }
+
+    fn parse_statement_item(&mut self) -> Option<Item> {
+        let stmt = self.parse_statement()?;
+        match &stmt {
+            Statement::Function(f) => {
+                self.validate_function_definition(f);
+            }
+            _ => {}
+        }
+        Some(stmt.into())
     }
 }
